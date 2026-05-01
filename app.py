8d2801cf77d2e880bd7e8c1e8509233af4ea53a0
Zakaria <zakariaelatia98@gmail.com>
2026-05-01 16:32:29 +0100

    add html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Zakaria 😎 I LOVE ATIKA"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
