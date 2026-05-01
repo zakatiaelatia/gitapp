d4563066c68c8589e3be8d82004fe75064b06236
Zakaria <zakariaelatia98@gmail.com>
2026-05-01 20:41:38 +0100

    add products api

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+# هادي اللي ناقصة ❗
+@app.route("/products")
+def products():
+    data = ["PC Gamer", "iPhone", "Keyboard"]
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
